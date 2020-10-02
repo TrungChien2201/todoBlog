eb3dd088c26621765334431daf662e9688455ed5
TrungChien2201 <chienvu.neon@gmail.com>
2020-10-02 11:49:51 +0700

    update

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import '../styles/index.scss'
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+    className="d-flex header-menu"
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <Link to="/">Home</Link> 
+      <Link to="/page-2/">Admin</Link> 
+      <Link to="/blog/">Blog</Link>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
